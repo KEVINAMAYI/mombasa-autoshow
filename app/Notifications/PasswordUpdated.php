7d893d7ae4eb3162bf4c4ffa65803699814dfe5a
KEVINAMAYI <kevinamayi20@gmail.com>
2024-11-18 17:40:06 +0300

    Add Queue For Password and Activation Emails...

--- app/Notifications/PasswordUpdated.php
+++ app/Notifications/PasswordUpdated.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PasswordUpdated extends Notification
+class PasswordUpdated extends Notification implements ShouldQueue
 {
     use Queueable;
 
