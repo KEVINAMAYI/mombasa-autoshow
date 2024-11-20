8195c7ea17c42dbe88c4f47e9dcc984a37460dcd
KEVINAMAYI <kevinamayi20@gmail.com>
2024-11-20 18:59:43 +0300

    Use Que for Email signup...
    (cherry picked from commit ee53ee05a24d5e8e42945c2b5f969559db76f101)

---
+++ app/Notifications/CustomVerifyEmail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomVerifyEmail extends VerifyEmail implements ShouldQueue
+{
+    use Queueable;
+
+    public function toMail($notifiable)
+    {
+
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verify Your Email Address')
+            ->line('Please click the button below to verify your email address.')
+            ->action('Verify Email Address', $verificationUrl)
+            ->line('If you did not create an account, no further action is required.');
+    }
+
+}
