ff6ddc76d5713c590030b17ed2489619f982079e
KEVINAMAYI <kevinamayi20@gmail.com>
2024-11-24 21:02:34 +0300

    Update Email Header...

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Notifications\CustomVerifyEmail;
+use App\Notifications\VerifyEmailAddress;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -77,6 +77,6 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function sendEmailVerificationNotification()
     {
-        $this->notify(new CustomVerifyEmail());
+        $this->notify(new VerifyEmailAddress());
     }
 }
--- app/Notifications/VerifyEmailAddress.php
+++ app/Notifications/VerifyEmailAddress.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class CustomVerifyEmail extends VerifyEmail implements ShouldQueue
+class VerifyEmailAddress extends VerifyEmail implements ShouldQueue
 {
     use Queueable;
 
